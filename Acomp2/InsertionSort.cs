f68081bc738ace547d44e0a9acc4ff3e4440144c
Sávio Da Silva Lopes <20171ENEL0322@cefetes.br>
2019-08-16 17:48:33 -0300

    terminei

--- Acomp2/InsertionSort.cs
+++ Acomp2/InsertionSort.cs
@@ -4,7 +4,28 @@
     {
         public void Sort(int[] E, int n)
         {
-            throw new System.NotImplementedException();
+            int eleito,j;
+            for (int i = 0; i < n; i++) //(1)
+            {
+                eleito = E[i];
+                j = i - 1;
+                while ((j >= 0) && (eleito < E[j]))
+                {
+
+                    E[j + 1] = E[j];
+                    j = j - 1;
+                }
+
+
+
+                if (j != i) { 
+
+                    E[j + 1] = eleito;
+
+                }
+
+            }
         }
+
     }
 }
--- Acomp2/SelectionSort.cs
+++ Acomp2/SelectionSort.cs
@@ -4,7 +4,25 @@
     {
         public void Sort(int[] E, int n)
         {
-            throw new System.NotImplementedException();
+            int k, x;
+
+            for (int i = 0; i < n; i++) //(1)
+            {
+                k = i;
+                x = E[i];
+                for (int j = i + 1; j < n; j++) //(2)
+                {
+                    if (E[j] < x) //(3)
+                    {
+                        k = j;
+                        x = E[k];
+                    }
+                }
+                E[k] = E[i];
+                E[i] = x;
+
+            }
         }
+
     }
 }
